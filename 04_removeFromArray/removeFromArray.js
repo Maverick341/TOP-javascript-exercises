4717680f74214407c7e94ec311cbcf66d2cb4975
Maverick341 <shinde341sushant@gmail.com>
2024-09-14 21:15:11 +0530

    solved 04 exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,19 @@
 const removeFromArray = function() {
+    const args = Array.from(arguments);
+
+    const array = args[0];
+
+    const valuesToRemove = args.slice(1);
+
+    const newArray = [];
+
+    for (let item = 0; item < array.length; item++) {
+        if (!valuesToRemove.includes(array[item])) {
+            newArray.push(array[item]);
+        }
+    }
+
+    return newArray;
 };
 
 // Do not edit below this line

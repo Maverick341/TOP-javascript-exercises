7bdaf56c43c4394626b89d3f34d051051be673cd
Maverick341 <shinde341sushant@gmail.com>
2024-09-15 19:02:08 +0530

    solved 7th exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,12 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (F) {
+  let value = (F - 32) / 1.8;
+  return Math.round(value*10)/10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (C) {
+  let value = 1.8*C + 32;
+  return Math.round(value*10)/10;
+  
 };
 
 // Do not edit below this line
